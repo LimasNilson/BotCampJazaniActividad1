de33a846b6a00ea8cb88de4782251456e155eeea
Nilson <82770534+LimasNilson@users.noreply.github.com>
2023-10-03 16:40:58 -0500

    first

---
+++ Jazani.Api/Controllers/Admins/AlertController.cs
@@ -0,0 +1,56 @@
+﻿using Jazani.Application.Admins.Dtos.Alert;
+using Jazani.Application.Admins.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Jazani.Api.Controllers.Admins
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlertController : ControllerBase
+    {
+
+        private readonly IAlertService _alertService;
+
+        public AlertController(IAlertService alertrService)
+        {
+            _alertService = alertrService;
+        }
+
+        
+        [HttpGet]
+        public async Task<IReadOnlyList<AlertDto>> Get()
+        {
+            return await _alertService.FindAllAsync();
+        }
+
+        
+        [HttpGet("{id}")]
+        public async Task<AlertDto?> Get(int id)
+        {
+            return await _alertService.FindByIdAsync(id);
+        }
+
+        
+        [HttpPost]
+        public async Task<AlertDto> Post([FromBody] AlertSaveDto alertSaveDto)
+        {
+            return await _alertService.CreateAsync(alertSaveDto);
+        }
+
+        
+        [HttpPut("{id}")]
+        public async Task<AlertDto> Put(int id, [FromBody] AlertSaveDto alertSaveDto)
+        {
+            return await _alertService.EditAsync(id, alertSaveDto);
+        }
+
+        
+        [HttpDelete("{id}")]
+        public async Task<AlertDto> Delete(int id)
+        {
+            return await _alertService.DisabledAsync(id);
+        }
+    }
+}
---
+++ Jazani.Api/Controllers/Admins/NatureController.cs
@@ -0,0 +1,55 @@
+﻿using Jazani.Application.Admins.Dtos.Periocities;
+using Jazani.Application.Admins.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Jazani.Api.Controllers.Admins
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NatureController : ControllerBase
+    {
+        private readonly INatureService _natureService;
+
+        public NatureController(INatureService natureService)
+        {
+            _natureService =natureService;
+        }
+
+       
+        [HttpGet]
+        public async Task<IEnumerable<NatureDto>> Get()
+        {
+            return await _natureService.FindAllAsync();
+        }
+
+        
+        [HttpGet("{id}")]
+        public async Task<NatureDto?> Get(int id)
+        {
+            return await _natureService.FindByIdAsync(id);
+        }
+
+        
+        [HttpPost]
+        public async Task<NatureDto> Post([FromBody] NatureSaveDto natureSaveDto)
+        {
+            return await _natureService.CreateAsync(natureSaveDto);
+        }
+
+     
+        [HttpPut("{id}")]
+        public async Task<NatureDto> Put(int id, [FromBody] NatureSaveDto natureSaveDto)
+        {
+            return await _natureService.EditAsync(id, natureSaveDto);
+        }
+
+        
+        [HttpDelete("{id}")]
+        public async Task<NatureDto> Delete(int id)
+        {
+            return await _natureService.DisabledAsync(id);
+        }
+    }
+}
